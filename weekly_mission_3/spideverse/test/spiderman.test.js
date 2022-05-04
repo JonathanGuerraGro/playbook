ebc50320bf6d4b1b9a4ecd5ba486e2caad5dcc9e
CJonathan Guerra <alexguerragro@gmail.com>
2022-05-04 11:30:54 -0500

    Modificacion de la prueba de unidad agregando descripciones mas explicitas segun los requerimientos de la clase spiderman

--- weekly_mission_3/spideverse/test/spiderman.test.js
+++ weekly_mission_3/spideverse/test/spiderman.test.js
@@ -1,6 +1,12 @@
 describe("Test Suite Dummy Description", () => {
     test('Case 1 Dummy', () => {
-        const resultOfSomething = 1 + 2
-        expect(resultOfSomething).toBe(18);
+
+       const andrewGarfield = new Spider ("Spiderman Sony", 31, "Andrew Garfield", 2, "Sony")
+
+       expect(andrewGarfield.name).toBe("Spiderman Sony")
+       expect(andrewGarfield.age).toBe(31)
+       expect(andrewGarfield.actor).toBe("Andrew Garfield")
+       expect(andrewGarfield.movies.movies).toBe(31)
+       expect(andrewGarfield.movies.studio).toBe("Sony")
     });
 })
