e9057b46162e1d68eb6f897732d918e89c2ecde6
CJonathan Guerra <alexguerragro@gmail.com>
2022-05-08 17:30:18 -0500

    Se desarrolla el requerimiento creando una clase User en la prueba de unidad con Jest para instanciar un objeto poniendo las propiedades de mi User

---
+++ weekly_mission_3/twitter/test/models/User.test.js
@@ -0,0 +1,19 @@
+const User = require ('./../../app/models/User')
+
+describe("Unit Test for User class"), () => {
+
+    test ('Create an User object'), () => {
+        //Aqui se invoca el codigo que se usara en la app
+        const user = new User(1, "jonathan guerra", "Jonathan", "Bio", "dateCreated", "lastUpdated")
+
+        //Aqui validamos los resultado del codigo
+        // Esta es una comparación que va a igualar el valor de la izquierda con el valor de la derecha (valor esperado)
+
+        expect(user.id).toBe(1)
+        expect(user.username).toBe("jonathan guerra")
+        expect(user.name).toBe("Jonathan")
+        expect(user.bio).toBe("Bio")
+        expect(user.dateCreated).toBe("dateCreated")
+        expect(user.lastUpdated).toBe("lastUpdated")
+    }
+}
