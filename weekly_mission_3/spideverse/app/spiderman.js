37dd263770c7e20cb7bc7da2153edc620c4ea3aa
CJonathan Guerra <alexguerragro@gmail.com>
2022-05-08 15:34:44 -0500

    Se utilizaron getters para agregar el requerimiento de un nuevo spiderman al proyecto

--- weekly_mission_3/spideverse/app/spiderman.js
+++ weekly_mission_3/spideverse/app/spiderman.js
@@ -1,3 +1,16 @@
 class Spiderman{
-constructor(name, age, actor, movies, studio){this.name = name, this.age = age, this.actor = actor, this.movies = movies, this.studio = studio}}
+constructor(name, age, actor, movies, studio, info) {
+    this.name = name,
+    this.age = age,
+    this.actor = actor,
+    this.movies = movies,
+    this.studio = studio,
+    this.info = info
+}
+get getInfo(){
+    return this.info
+    }
+}
+
+
 module.exports = Spiderman
--- weekly_mission_3/spideverse/test/spiderman.test.js
+++ weekly_mission_3/spideverse/test/spiderman.test.js
@@ -4,11 +4,17 @@ describe("Unit Tests for Spiderman class", () => {
     test('Create an spiderman object', () => {
 
        const andrewGarfield = new Spiderman ("Spiderman Sony", 31, "Andrew Garfield", 2, "Sony")
-
        expect(andrewGarfield.name).toBe("Spiderman Sony")
        expect(andrewGarfield.age).toBe(31)
        expect(andrewGarfield.actor).toBe("Andrew Garfield")
        expect(andrewGarfield.movies).toBe(2)
        expect(andrewGarfield.studio).toBe("Sony")
-    });
+    })
+
+
+
+test('2) Use the method getInfo()', () => {
+    const tomHolland = new Spiderman("Spiderman Marvel", 25, "Tom Holland", 5, "Marvel", "Hey, I'm Tom Holland from Marvel studio")
+    expect(tomHolland.getInfo).toBe("Hey, I'm Tom Holland from Marvel studio")
+    })
 })
